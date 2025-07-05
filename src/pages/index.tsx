bedaafc77a015cb24a8b88f65ff991b6d6674d31
nnna <nagidec.24a@gmail.com>
2025-07-05 23:57:31 +0900

    refactor: プロジェクトステータスに関するコメントを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -268,7 +268,7 @@ function ProjectShowcase() {
     {
       title: "Tydying up",
       description: "難易度高めの3Dアクションゲーム．様々な場所にあるおもちゃを操作しておもちゃ箱に片づけるゲームです．",
-      status: "開発中",
+      status: "開発中",  // 開発中 | 公開中 | 企画中
       technologies: ["Unity", "C#", "Blender"]
     },
   ];
